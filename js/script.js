897d029dc8ff96fc576d704d8eb37b0b94fc906f
Oriyan Nadav <oriyanadav5@gmail.com>
2023-08-20 22:03:36 +1000

    scroll reveal

--- js/script.js
+++ js/script.js
@@ -33,3 +33,14 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
