060c165eb047c232c84ff6cc326bdf8578b031da
Oriyan Nadav <oriyanadav5@gmail.com>
2023-08-20 22:03:51 +1000

    typed js

--- js/script.js
+++ js/script.js
@@ -44,3 +44,12 @@ ScrollReveal({
 ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+
+/*==================== typed js ====================*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
