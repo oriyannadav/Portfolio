09fbd2a23c9879659c5fa8197f3be7340226cb79
Oriyan Nadav <oriyanadav5@gmail.com>
2023-09-02 21:51:32 +1000

    navbar animation fix

--- js/script.js
+++ js/script.js
@@ -19,9 +19,19 @@ menuBtn.addEventListener('click', () => {
     
 });
 
+const navLinks = document.querySelectorAll('header nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menuBtn.classList.remove('open'); // Change the menu button back to its initial state
+        menuOpen = false;
+        mobileNavbar.classList.remove('active'); // Close the mobile navbar
+    });
+});
+
 /*==================== scroll sections active link ====================*/
 let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+// let navLinks = document.querySelectorAll('header nav a');
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
